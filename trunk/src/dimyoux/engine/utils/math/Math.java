2846d417f3fa04dff501de967a830b2fe018e1ce
mymyoux <mymyoux@ea41417f-df01-accb-03a7-279b946013a4>
2011-03-21 16:38:51 +0000

    Converts Radians to degrees for Magnetic and Acceleration sensors

---
+++ trunk/src/dimyoux/engine/utils/math/Math.java
@@ -0,0 +1,14 @@
+package dimyoux.engine.utils.math;
+/**
+ * Math class
+ */
+public class Math {
+	/**
+	 * factor to convert radians to degrees
+	 */
+	public static final float RAD2DEG = (float) (180.0/java.lang.Math.PI);
+	/**
+	 * factor to convert degrees to radians
+	 */
+	public static final float DEG2RAD = (float) (java.lang.Math.PI/180.0);
+}
