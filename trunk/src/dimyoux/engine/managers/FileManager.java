866e41accbb35a5bc139f10ce0e63a31faf02c82
mymyoux <mymyoux@ea41417f-df01-accb-03a7-279b946013a4>
2011-03-26 09:44:35 +0000

    Some improvements in FileManager class

--- trunk/src/dimyoux/engine/managers/FileManager.java
+++ trunk/src/dimyoux/engine/managers/FileManager.java
@@ -1,19 +1,21 @@
 package dimyoux.engine.managers;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.content.res.Resources;
 import android.content.res.Resources.NotFoundException;
 
 import dimyoux.engine.R;
-import dimyoux.engine.utils.Log;
-import dimyoux.engine.utils.parsers.ObjParser;
 
 /**
  * File Manager
  */
 public class FileManager {
+	static public Resources resources;
 	/**
 	 * Instance [Singleton]
 	 */
@@ -61,13 +63,32 @@ public class FileManager {
 	/**
 	 * @return Return the FileManager instance
 	 */
-	public FileManager getInstance()
+	public static FileManager getInstance()
 	{
 		if(_instance == null)
 		{
 			_instance = new FileManager();
 		}
 		return _instance;
+	}
+	/**
+	 * Open a file as text file
+	 * @param name File's name
+	 * @param folder Folder's name
+	 * @return File
+	 */
+	public BufferedReader openTextFile(String name, String folder)
+	{
+		return new BufferedReader(new InputStreamReader(openFile(name, folder)));
+	}
+	/**
+	 * Open a file as text file
+	 * @param name File's name
+	 * @return File
+	 */
+	public BufferedReader openTextFile(String name)
+	{
+		return new BufferedReader(new InputStreamReader(openFile(name)));
 	}
 	//TODO:resources can't be in objParser !!!
 	/**
@@ -76,18 +97,8 @@ public class FileManager {
 	 */
 	public InputStream openFile(String name) throws NotFoundException
 	{
-		name = name.replace(".","_");
-		String packageName = this.packageName+":";
-		int id = 0;
-		for(final String folder : folders)
-		{
-			id = ObjParser.resources.getIdentifier(packageName+folder+"/"+name, null, null);
-			if(id > 0)
-			{
-				break;
-			}
-		}
-		return ObjParser.resources.openRawResource(id);
+		int id = getFileID(name);
+		return resources.openRawResource(id);
 	}
 	/**
 	 * Open a file
@@ -96,7 +107,75 @@ public class FileManager {
 	 */
 	public InputStream openFile(String name, String folder) throws NotFoundException
 	{
-		String packageName = this.packageName+":";
-		return ObjParser.resources.openRawResource(ObjParser.resources.getIdentifier(packageName+folder+"/"+name, null, null));
+		int id = getFileID(name, folder);
+		return resources.openRawResource(id);
+	}
+	/**
+	 * Gets file name associated with id
+	 * @param id ID of file
+	 * @return URL of file
+	 */
+	public String getFileName(int id)
+	{
+		return resources.getString(id);
+	}
+	/**
+	 * Gets ID of a file
+	 * @param name File's name
+	 * @return ID of the file. 0 if the file hasn't been found
+	 */
+	public int getFileID(String name)
+	{
+		if(name.startsWith("/"))
+		{
+			name = name.substring(1);
+		}
+		String packageName = FileManager.packageName+":";
+		int id = 0;
+		for(final String folder : folders)
+		{
+			id = resources.getIdentifier(packageName+folder+"/"+name, null, null);
+			if(id > 0)
+			{
+				break;
+			}
+		}
+		if(id==0)
+		{
+			name = name.replace(".","_");
+			for(final String folder : folders)
+			{
+				id = resources.getIdentifier(packageName+folder+"/"+name, null, null);
+				if(id > 0)
+				{
+					break;
+				}
+			}
+		}
+		return id;
+	}
+	/**
+	 * Gets ID of a file
+	 * @param name File's name
+	 * @param folder Folder where the file is
+	 * @return ID of the file. 0 if the file hasn't been found
+	 */
+	public int getFileID(String name, String folder)
+	{
+		if(name.startsWith("/"))
+		{
+			name = name.substring(1);
+		}
+		String packageName = FileManager.packageName+":";
+		int id;
+		try
+		{
+			id = resources.getIdentifier(packageName+folder+"/"+name, null, null);
+		}catch(NotFoundException e)
+		{
+			name = name.replace(".","_");
+			id = resources.getIdentifier(packageName+folder+"/"+name, null, null);
+		}
+		return id;
 	}
 }
