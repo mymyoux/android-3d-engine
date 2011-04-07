58ffb6ae261cd73bfc3537c4dcd8f236c71335bc
mymyoux <mymyoux@ea41417f-df01-accb-03a7-279b946013a4>
2011-04-07 22:56:10 +0000

    started TouchEvent management

--- trunk/src/dimyoux/engine/core/JDBGLSurfaceView.java
+++ trunk/src/dimyoux/engine/core/JDBGLSurfaceView.java
@@ -1,14 +1,19 @@
 package dimyoux.engine.core;
 
 
+import dimyoux.engine.managers.SensorManager;
+import dimyoux.engine.utils.Log;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 /**
  *  GLSurfaceView 
  */
 
 public class JDBGLSurfaceView extends GLSurfaceView {
 
+	private GestureDetector gestureDetector;
 	/**
 	 * Renderer
 	 */
@@ -20,9 +25,16 @@ public class JDBGLSurfaceView extends GLSurfaceView {
 	 */
 	public JDBGLSurfaceView(Context context, GLSurfaceView.Renderer renderer) {
 		super(context);
+		gestureDetector = new GestureDetector(context, SensorManager.getInstance());
 		this.renderer = renderer;
 		setRenderer(renderer);
+		 
 	}
 
 
+	@Override
+    public boolean onTouchEvent(final MotionEvent event) {
+    	 gestureDetector.onTouchEvent(event);
+    	return true;
+    }
 }
